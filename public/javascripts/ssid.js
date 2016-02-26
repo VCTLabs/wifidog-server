4080349dc548109c6e9374118e9fff12502400ac
root <root@beaglebone.localdomain>
2016-02-26 03:41:41 +0000

    add socket.io, scan ssid function

--- public/javascripts/ssid.js
+++ public/javascripts/ssid.js
@@ -1,7 +1,20 @@
 $(function(){
-    $("#ssid").append("<option value='1'>hello</option>");
+    //$("#ssid").append("<option value='3'>hello</option>");
     var iosocket = io();
-    iosocket.on('message', function(message) {
-                        $('#incomingChatMessages').append($('<li></li>').text(message));
+    iosocket.on('wlan0_info', function(message) {
+    if(message.length > 0)
+    {
+        var obj = JSON.parse(message); 
+        //alert(message);
+        $('#ssid').empty();
+        for (var i = 0; i < obj.length; i++) {
+            $('#ssid').append($("<option value='1'>"+
+                        obj[i].ssid+'    '+obj[i].signal+
+                        "</option>"));
+        }                
+    }
     });
-})
+    $("#ssid").click(function(){
+        iosocket.emit('rescan_wlan0_info','a');
+    });
+});
