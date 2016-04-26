85f1fe3cbdb1cd66ad55ae5088bc720c4b530fe3
Baozhu Zuo <zuobaozhu@gmail.com>
2016-04-26 04:14:28 +0000

    change wifidog-server to webconfig

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var users = require(__dirname + '/routes/users');
 var ping = require(__dirname + '/routes/ping');
 
 var app = express();
-var b = require('bonescript');
+// var b = require('bonescript');
 
-b.serverStart();
+// b.serverStart();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', ejs.__express);
